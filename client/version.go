0813139f2b0c2e5eb56431ed8138e12f0ea99ccc
MurphyMarkW <murphy.mark.w+github@gmail.com>
2017-05-28 23:57:20 -0500

    use git tag for version string

--- client/version.go
+++ client/version.go
@@ -1,3 +1,6 @@
 package client
 
-const Version = "0.0.1"
+// NOTE This variable will be replaced at travis build time with the tag
+// currently pointing to HEAD in the git repo. See the .travis-ci.yml for
+// specifics on how it is built.
+var Version = "dev"
